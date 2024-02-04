0f9faae0f5d5533920b78f28e16cbdc7f8590ae5
Shaun <Hundin231@gmail.com>
2024-02-04 20:55:08 +0800

    Added server shit boys

---
+++ Server/models/volunteers.js
@@ -0,0 +1,103 @@
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const addressSchema = new schema({
+  city: String,
+  street: String,
+  houseNumber: String,
+  postal_code: String
+});
+
+const volunteerSchema =  new schema({
+    // Common fields for all users (base user is defined as a volunteer)
+    // if isAdmin then will have access to admin pages
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    // Volunteer-specific fields
+    firstName: {
+      type: String,
+      required: true
+
+    },
+    lastName: {
+      type: String,
+      required: true
+
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],
+      required: true,
+
+    },
+
+    education: {
+      type: String,
+      required: true,
+    },
+
+    address: {
+        type: addressSchema,
+        required: true
+    },
+
+    dateOfBirth: {
+      type: Date,
+      required: true
+    },
+
+    residentialStatus: {
+      type: String,
+      enum: ['Singapore PR', 'Singaporean Citizen', 'Long term visit pass', 'Employment pass or SPass'],
+      required: true
+    },
+
+    skills: {
+      type: [String],
+      default: [],
+      required: true
+    },
+
+    pastExperiences: {
+      type: String,
+      required: true
+    },
+
+    volunteerPreferences: {
+      type: String,
+      required: true
+    },
+    userStatus: {
+        type: String,
+        enum: ['unverified', 'active', 'inactive', 'banned', 'deleted'],
+        required: true
+    },
+    userPersonalityResult: {
+      type: String,
+      enum: [],
+      required: false
+    },
+    userRole: {
+      type: String,
+      enum: ['admin', 'volunteer']
+    }
+
+  });
+  const volunteer = mongoose.model('Volunteer', volunteerSchema);
+  module.exports = volunteer;
+
