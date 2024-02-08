f4d258dce577826f25bda441b6ffd43e2c8693c7
wenjebs <wenjieb2@gmail.com>
2024-02-08 20:12:34 +0800

    Style signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -90,191 +90,208 @@ export default function SignUp() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <NavBar></NavBar>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
+      <div style={{ backgroundColor: "#FBE3E0", paddingTop: "1rem" }}>
+        <Container
+          component="main"
+          maxWidth="xs"
           sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            backgroundColor: "white",
+            borderRadius: "40px",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            sx={{
+              pt: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="phone"
-                  label="WhatsApp Phone Number"
-                  name="phone"
-                  autoComplete="phone"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="Date of Birth"
-                    value={value}
-                    onChange={(newValue) => setValue(newValue)}
-                  ></DatePicker>
-                </LocalizationProvider>
-              </Grid>
-              <Grid item xs={12}>
-                <Box sx={{ minWidth: 120 }}>
-                  <FormControl fullWidth>
-                    <InputLabel id="gender">Gender</InputLabel>
-                    <Select
-                      labelId="gender"
-                      id="gender"
-                      value={gender}
-                      label="gender"
-                      onChange={handleGender}
-                    >
-                      <MenuItem value={"Male"}>Male</MenuItem>
-                      <MenuItem value={"Female"}>Female</MenuItem>
-                      <MenuItem value={"Other"}>Other</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Box>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  name="occupation"
-                  label="Occupation"
-                  type="occupation"
-                  id="occupation"
-                  autoComplete="occupation"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Currently Studying?"
-                  name="studying"
-                  onChange={handleStudyCheckbox}
-                />
-              </Grid>
-              {studying.studying && (
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{
+                textAlign: "center",
+                py: 4,
+                fontWeight: "bolder",
+                fontSize: "3rem",
+              }}
+            >
+              Join us today!
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
                 <Grid item xs={12}>
                   <TextField
                     required
                     fullWidth
-                    id="studyingField"
-                    label="Where are you studying?"
-                    name="studyingField"
-                    autoComplete="off"
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
                   />
                 </Grid>
-              )}
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Can you drive?"
-                  name="canDrive"
-                  onChange={handleDriving}
-                />
-              </Grid>
-              {canDrive.canDrive && (
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="phone"
+                    label="WhatsApp Phone Number"
+                    name="phone"
+                    autoComplete="phone"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="Date of Birth"
+                      value={value}
+                      onChange={(newValue) => setValue(newValue)}
+                    ></DatePicker>
+                  </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="gender">Gender</InputLabel>
+                      <Select
+                        labelId="gender"
+                        id="gender"
+                        value={gender}
+                        label="gender"
+                        onChange={handleGender}
+                      >
+                        <MenuItem value={"Male"}>Male</MenuItem>
+                        <MenuItem value={"Female"}>Female</MenuItem>
+                        <MenuItem value={"Other"}>Other</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    name="occupation"
+                    label="Occupation"
+                    type="occupation"
+                    id="occupation"
+                    autoComplete="occupation"
+                  />
+                </Grid>
                 <Grid item xs={12}>
                   <FormControlLabel
                     control={<Checkbox />}
-                    label="Do you own a vehicle?"
-                    name="ownVehicle"
+                    label="Currently Studying?"
+                    name="studying"
+                    onChange={handleStudyCheckbox}
                   />
                 </Grid>
-              )}
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="EducationBackground"
-                  label="Educational background"
-                  type="EducationBackground"
-                  id="EducationBackground"
-                />
+                {studying.studying && (
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="studyingField"
+                      label="Where are you studying?"
+                      name="studyingField"
+                      autoComplete="off"
+                    />
+                  </Grid>
+                )}
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Can you drive?"
+                    name="canDrive"
+                    onChange={handleDriving}
+                  />
+                </Grid>
+                {canDrive.canDrive && (
+                  <Grid item xs={12}>
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label="Do you own a vehicle?"
+                      name="ownVehicle"
+                    />
+                  </Grid>
+                )}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="EducationBackground"
+                    label="Educational background"
+                    type="EducationBackground"
+                    id="EducationBackground"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/signin" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/signin" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </div>
     </ThemeProvider>
   );
 }
